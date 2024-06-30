859d167bfa8731e19edf5a0f1faff7dd28dd690f
Eiad-Sorour <eiad.sorour@yahoo.com>
2024-06-30 05:11:14 +0300

    errors_state_added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -20,13 +20,59 @@ function RegisterPage(){
         }
     );
 
+    const [errors, setErrors] = React.useState(
+        {
+            serverError: "",
+            passwordError: "",
+            confirmPassClass: "form-control"
+        }
+    );
+
     function hanldeOnChange(event) {
         const changeName = event.target.id;
+        const currentValue = event.target.value;
         setUserData((prev)=>{
+            if(changeName === "confirmPassword"){
+                if(currentValue === prev.password){
+                    setErrors(
+                        {
+                            ...prev,
+                            passwordError: "",
+                            confirmPassClass: "form-control"
+                        }
+                    )
+                }else{
+                    setErrors(
+                        {
+                            ...prev,
+                            passwordError: "Password must match",
+                            confirmPassClass: "form-control border border-3 border-danger focus-ring focus-ring-danger"
+                        }
+                    )
+                }
+            }else if(changeName === "password"){
+                if(currentValue === prev.confirmPassword){
+                    setErrors(
+                        {
+                            ...prev,
+                            passwordError: "",
+                            confirmPassClass: "form-control"
+                        }
+                    )
+                }else{
+                    setErrors(
+                        {
+                            ...prev,
+                            passwordError: "Password must match",
+                            confirmPassClass: "form-control border border-3 border-danger focus-ring focus-ring-danger"
+                        }
+                    )
+                }
+            }
             return(
                 {
                     ...prev,
-                    [changeName]: event.target.value
+                    [changeName]: currentValue
                 }
             )
         })
@@ -74,6 +120,7 @@ function RegisterPage(){
     return (
         <main className="form-signin m-auto position-absolute top-50 start-50 translate-middle">
             <form className="container text-center ">
+                <p className="m-0" style={{color:"red"}}>{errors.serverError}</p>
                 <div className="row">
                     <h1 className="h3 mb-3 fw-bold">Sign Up</h1>
                 </div>
@@ -89,20 +136,23 @@ function RegisterPage(){
                     </div>
                 </div>
                 <div className="row">
-                    <div className="form-floating p-0 col mx-1 mb-2">
-                        <input style={{minWidth: "120px"}} onChange={hanldeOnChange} type={passwordTypes.passwordType} className="form-control" id="password" placeholder="Password"/>
-                        <label htmlFor="password">Password</label>
-                        <button onClick={handleOnClick} id="btnPassword" className="bg-transparent border-0 position-absolute top-50 end-0 translate-middle-y me-1" ><i id="passIcon" className={passwordTypes.passIcon}></i></button>
+                    <div className="p-0 col mx-1 mb-2">
+                        <div className="form-floating">
+                            <input style={{minWidth: "120px"}} onChange={hanldeOnChange} type={passwordTypes.passwordType} className="form-control" id="password" placeholder="Password"/>
+                            <label htmlFor="password">Password</label>
+                            <button onClick={handleOnClick} id="btnPassword" className="bg-transparent border-0 position-absolute top-50 end-0 translate-middle-y me-1" ><i id="passIcon" className={passwordTypes.passIcon}></i></button>
+                        </div>
                     </div>
-                    <div className="form-floating p-0 col mx-1 mb-2">
-                        <input style={{minWidth: "190px"}} onChange={hanldeOnChange} type={passwordTypes.confirmPasswordType} className="form-control " id="confirmPassword" placeholder="Confirm Password"/>
-
-                        <label htmlFor="confirmPassword">Confirm Password</label>
-                        <button onClick={handleOnClick} id="btnConfirm" className="bg-transparent border-0 position-absolute top-50 end-0 translate-middle-y me-1" ><i id="confirmIcon" className={passwordTypes.confirmPassIcon}></i></button>
+                    <div className="p-0 col mx-1 mb-2">
+                        <div className="form-floating">
+                            <input style={{minWidth: "190px"}} onChange={hanldeOnChange} type={passwordTypes.confirmPasswordType} className={errors.confirmPassClass} id="confirmPassword" placeholder="Confirm Password"/>
+                            <label htmlFor="confirmPassword">Confirm Password</label>
+                            <button onClick={handleOnClick} id="btnConfirm" className="bg-transparent border-0 position-absolute top-50 end-0 translate-middle-y me-1" ><i id="confirmIcon" className={passwordTypes.confirmPassIcon}></i></button>    
+                        </div>
+                        <p className="m-0" style={{color:"red"}}>{errors.passwordError}</p>
                     </div>
                 </div>
 
-                <p className="m-0" style={{color:"red"}}></p>
 
                 <div className="row mb-1">
                     <button className="btn btn-primary fw-bold w-100 py-2 mt-3" type="submit">Sign Up</button>
