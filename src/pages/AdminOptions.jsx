9df09dbe406c50320777bd97921733932c3e85b8
Eiad-Sorour <eiad.sorour@yahoo.com>
2024-07-03 03:56:38 +0300

    page_design_implemented

--- src/pages/AdminOptions.jsx
+++ src/pages/AdminOptions.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 function AdminOptionsPage(probs){
     return (
-        <h1>This is admin options page</h1>
+        <div class="text-center d-grid gap-2 col-3 mx-auto position-absolute top-50 start-50 translate-middle">
+            <h3>Welcome, <span className="text-decoration-underline fw-bold ">Eiad</span></h3>
+            <button class="btn btn-primary" type="button">Users</button>
+            <button class="btn btn-primary" type="button">Rooms Hirstory</button>
+            <a className="link-danger link-offset-2 link-offset-2-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href="">Logout</a>
+        </div>
     )
 }
 
