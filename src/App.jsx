b62fb1832119a12399d075e0ab7787b8c25f54e0
Eiad-Sorour <eiad.sorour@yahoo.com>
2024-07-05 00:52:17 +0300

    remove_urls_from_props

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,12 @@ import LoginPage from "./pages/Login";
 
 function App() {
 
-  const FRONT_URL = process.env.REACT_APP_FRONT_URL;
-  const BACK_URL = process.env.REACT_APP_BACK_URL;
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<RegisterPage BACK_URL={BACK_URL} FRONT_URL={FRONT_URL} />}/>
-        <Route path="/register" element={<RegisterPage BACK_URL={BACK_URL} FRONT_URL={FRONT_URL} />}/>
-        <Route path="/login" element={<LoginPage BACK_URL={BACK_URL} FRONT_URL={FRONT_URL}/>}/>
+        <Route index element={<RegisterPage />}/>
+        <Route path="/register" element={<RegisterPage />}/>
+        <Route path="/login" element={<LoginPage />}/>
       </Routes>
     </BrowserRouter>
   );
