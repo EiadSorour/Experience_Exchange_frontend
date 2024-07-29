f21a1a5609969ec6e04f6fd73a71d8e6f6118c4e
Eiad-Sorour <eiad.sorour@yahoo.com>
2024-07-29 04:34:10 +0300

    rooms_history_base_design

--- src/pages/RoomsHistory.jsx
+++ src/pages/RoomsHistory.jsx
@@ -46,36 +46,172 @@ function RoomsHistoryPage(){
     }
 
     return (
-        <div className="text-center d-grid gap-2 col-11 mx-auto position-absolute top-50 start-50 translate-middle">
+        <div className="container text-center mx-auto position-absolute top-50 start-50 translate-middle">
             
-            <h6>Search by:</h6>
+            <div className="row">
+                <h6>Search by:</h6>
+            </div>
             
-            <div>
-                <div className="text-start form-check form-check-inline">
-                    <input checked={radioState.roomID} onClick={handleOnRadioChange} className="form-check-input" type="radio" name="inlineRadioOptions" id="roomID" value="option1"/>
-                    <label className="form-check-label" for="roomID">roomID</label>
+            <div className="row mb-2 justify-content-center">
+                <div className="col">
+                    <div className="text-start form-check form-check-inline">
+                        <input checked={radioState.roomID} onClick={handleOnRadioChange} className="form-check-input" type="radio" name="inlineRadioOptions" id="roomID" value="option1"/>
+                        <label className="form-check-label" for="roomID">roomID</label>
+                    </div>
                 </div>
-                <div className="text-start form-check form-check-inline">
-                    <input checked={radioState.creatorID} onClick={handleOnRadioChange} className="form-check-input" type="radio" name="inlineRadioOptions" id="creatorID" value="option2"/>
-                    <label className="form-check-label" for="creatorID">creatorID</label>
+                <div className="col">
+                    <div className="text-start form-check form-check-inline col">
+                        <input checked={radioState.creatorID} onClick={handleOnRadioChange} className="form-check-input" type="radio" name="inlineRadioOptions" id="creatorID" value="option2"/>
+                        <label className="form-check-label" for="creatorID">creatorID</label>
+                    </div>
                 </div>
-                <div className="text-start form-check form-check-inline">
-                    <input checked={radioState.creatorUsername} onClick={handleOnRadioChange} className="form-check-input" type="radio" name="inlineRadioOptions" id="creatorUsername" value="option3"/>
-                    <label className="form-check-label" for="creatorUsername">creator username</label>
+                <div className="col">
+                    <div className="text-start form-check form-check-inline col">
+                        <input checked={radioState.creatorUsername} onClick={handleOnRadioChange} className="form-check-input" type="radio" name="inlineRadioOptions" id="creatorUsername" value="option3"/>
+                        <label className="form-check-label" for="creatorUsername">creator username</label>
+                    </div>
                 </div>
-                <div className="text-start form-check form-check-inline">
-                    <input checked={radioState.topic} onClick={handleOnRadioChange} className="form-check-input" type="radio" name="inlineRadioOptions" id="topic" value="option4"/>
-                    <label className="form-check-label" for="topic" >Topic</label>
+                <div className="col">
+                    <div className="text-start form-check form-check-inline col">
+                        <input checked={radioState.topic} onClick={handleOnRadioChange} className="form-check-input" type="radio" name="inlineRadioOptions" id="topic" value="option4"/>
+                        <label className="form-check-label" for="topic" >Topic</label>
+                    </div>
                 </div>
             </div>
 
-            <div class="d-flex px-5 py-2 col-6 position-relative top-50 start-50 translate-middle">
-                <input onChange={handleOnSearchChange} style={{minWidth: "120px"}} type="text" class="form-control" placeholder="Search"/>
-                <button onClick={handleOnSearch} class="btn btn-primary ms-2">Search</button>
-                <button onClick={handleOnReset} class="btn btn-secondary ms-2">Reset</button>
+
+
+            <div className="row mb-4 justify-content-center">
+                <input onChange={handleOnSearchChange} style={{maxWidth:"500px"}} type="text" class="form-control col" placeholder="Search"/>
+                <button onClick={handleOnSearch} class="btn btn-primary ms-2 col-sm-1">Search</button>
+                <button onClick={handleOnReset} class="btn btn-secondary ms-2 col-sm-1">Reset</button>
             </div>
-            
-            <Link to={"/admin/options"} className="btn btn-primary position-absolute top-100 start-0 mt-5">Back</Link>
+
+
+
+
+            <div class="row ">
+                <div class="col-8">
+                    <table class="table table-bordered table-striped">
+                        <thead className="table-secondary">
+                            <tr>
+                                <th scope="col">RoomID</th>
+                                <th scope="col">Topic</th>
+                                <th scope="col">CreatorID</th>
+                                <th scope="col">Creator Username</th>
+                                <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody id="tbody">
+                            <tr>
+                                <td>b5a4c8ee-ef2f-4c98-97ec-3706fc81e843</td>
+                                <td>Firefighting Strategies</td>
+                                <td>550e8400-e29b-41d4-a716-44665544000f</td>
+                                <td>Eiad Sorour</td>
+                                <td><button className="btn btn-success">Users Joined</button></td>
+                            </tr>
+                            <tr>
+                                <td>b5a4c8ee-ef2f-4c98-97ec-3706fc81e843</td>
+                                <td>Firefighting Strategies</td>
+                                <td>550e8400-e29b-41d4-a716-44665544000f</td>
+                                <td>Eiad Sorour</td>
+                                <td><button className="btn btn-success">Users Joined</button></td>
+                            </tr>
+                            <tr>
+                                <td>b5a4c8ee-ef2f-4c98-97ec-3706fc81e843</td>
+                                <td>Firefighting Strategies</td>
+                                <td>550e8400-e29b-41d4-a716-44665544000f</td>
+                                <td>Eiad Sorour</td>
+                                <td><button className="btn btn-success">Users Joined</button></td>
+                            </tr>
+                            <tr>
+                                <td>b5a4c8ee-ef2f-4c98-97ec-3706fc81e843</td>
+                                <td>Firefighting Strategies</td>
+                                <td>550e8400-e29b-41d4-a716-44665544000f</td>
+                                <td>Eiad Sorour</td>
+                                <td><button className="btn btn-success">Users Joined</button></td>
+                            </tr>
+                            <tr>
+                                <td>b5a4c8ee-ef2f-4c98-97ec-3706fc81e843</td>
+                                <td>Firefighting Strategies</td>
+                                <td>550e8400-e29b-41d4-a716-44665544000f</td>
+                                <td>Eiad Sorour</td>
+                                <td><button className="btn btn-success">Users Joined</button></td>
+                            </tr>
+                            {/* {users.map((user, index)=>{
+                                return (
+                                    <tr>
+                                        <td>{user.userID}</td>
+                                        <td>{user.username}</td>
+                                        <td>{user.profession}</td>
+                                        <td>{user.role}</td>
+                                        <td name="button"><button id={`block_${index}`} onClick={showConfirmation.bind(this , [user])} className={user.isBlocked === false? "btn btn-danger":"btn btn-success"}>{user.isBlocked === true? "unblock": "Block"}</button></td>
+                                        <td name="button"><button id={`delete_${index}`} onClick={showConfirmation.bind(this,[user])} className="btn btn-danger">Delete</button></td>
+                                        <td name="button"><button id={`admin_${index}`} onClick={showConfirmation.bind(this,[user])} className={user.role === "admin"? "btn btn-danger":"btn btn-success"}>{user.role === "admin"? "Remove Admin":"Make Admin"}</button></td>
+                                    </tr>
+                                )
+                            })} */}
+                        </tbody>
+                    </table>
+
+                    <nav aria-label="Page navigation example">
+                        <ul className="pagination justify-content-center">
+                            <li class="page-item">
+                                <a class="page-link" href="#" aria-label="Previous">
+                                    <span aria-hidden="true">&laquo;</span>
+                                </a>
+                            </li>
+                            <li class="page-item"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item">
+                                <a class="page-link" href="#" aria-label="Next">
+                                    <span aria-hidden="true">&raquo;</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                
+                </div>
+                <div style={{maxHeight:"450px"}} class="col overflow-auto">
+                    <table  class="table table-bordered table-striped ">
+                        <thead className="table-secondary">
+                            <tr>
+                                <th scope="col">userID</th>
+                                <th scope="col">Username</th>
+                            </tr>
+                        </thead>
+                        <tbody id="tbody">
+                            <tr>
+                                <td>b5a4c8ee-ef2f-4c98-97ec-3706fc81e843</td>
+                                <td>Eiad Sorour</td>
+                            </tr>
+                            <tr>
+                                <td>b5a4c8ee-ef2f-4c98-97ec-3706fc81e843</td>
+                                <td>Eiad Sorour</td>
+                            </tr>
+                            <tr>
+                                <td>b5a4c8ee-ef2f-4c98-97ec-3706fc81e843</td>
+                                <td>Eiad Sorour</td>
+                            </tr>
+                            <tr>
+                                <td>b5a4c8ee-ef2f-4c98-97ec-3706fc81e843</td>
+                                <td>Eiad Sorour</td>
+                            </tr>
+                            <tr>
+                                <td>b5a4c8ee-ef2f-4c98-97ec-3706fc81e843</td>
+                                <td>Eiad Sorour</td>
+                            </tr>
+                            <tr>
+                                <td>b5a4c8ee-ef2f-4c98-97ec-3706fc81e843</td>
+                                <td>Eiad Sorour</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+            <div className="row justify-content-center">
+                <Link to={"/admin/options"} className="col-sm-1 btn btn-primary">Back</Link>
+            </div>
         </div>
     )
 }
