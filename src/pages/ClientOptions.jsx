a494c8908f176b3153266b80fe8ef0177ab9934e
Your Name <you@example.com>
2024-08-09 22:46:45 +0300

    design_implemented

--- src/pages/ClientOptions.jsx
+++ src/pages/ClientOptions.jsx
@@ -23,9 +23,41 @@ function ClientOptionsPage(){
     }
 
     return (
-        <div className="text-center d-grid gap-2 col-3 mx-auto position-absolute top-50 start-50 translate-middle">
-            <h1>Welcome, <span className="text-decoration-underline fw-bold ">{user.username}</span></h1>
-            <a onClick={handleOnLogout} role="button" className="link-danger link-offset-2 link-offset-2-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover">Logout</a>
+        <div>
+            <div className="d-inline-flex align-items-center p-2">
+                <h1>Welcome, <span className="text-decoration-underline fw-bold me-3">{user.username}</span></h1>
+                <a onClick={handleOnLogout} role="button" className="link-danger link-offset-2 link-offset-2-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover">Logout</a>
+            </div>
+
+            <div className="text-center d-grid gap-2 col-11 mx-auto position-absolute top-50 start-50 translate-middle">
+            
+                <div class="d-flex px-5 py-2 col-6 position-relative top-50 start-50 translate-middle">
+                    <input style={{minWidth: "120px"}} type="text" class="form-control" placeholder="Search by topic"/>
+                    <button class="btn btn-primary ms-2">Search</button>
+                    <button class="btn btn-secondary ms-2">Reset</button>
+                </div>
+                
+                <div style={{overflowY:"scroll", height:"450px"}}>
+                    <table class="table table-responsive table-borderless align-middle caption-top">
+                        <caption>Available rooms</caption>
+                        <tbody>
+                            <tr>
+                                <td scope="row"><span className="fw-bold text-danger">Room Topic :</span> this is topic</td>
+                                <td rowSpan={3}><button className="btn btn-success">Ask to join</button></td>
+                            </tr>
+                            <tr>
+                                <td scope="row"><span className="fw-bold text-danger">Creator:</span> eiad tarek</td>
+                            </tr>
+                            <tr>
+                                <td scope="row"><span className="fw-bold text-danger">Creator profession:</span> ai engineer</td>
+                            </tr>
+                            <tr><td colSpan={2}><hr/></td></tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+            <button className="btn btn-primary m-5 position-absolute bottom-0 end-0">Create Room</button>
         </div>
     )
 }
