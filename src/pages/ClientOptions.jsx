eb5173cfa5c513baac475b5d399c1c9b4f309073
Your Name <you@example.com>
2024-08-17 04:01:50 +0300

    roomDesign_code_modified

--- src/pages/ClientOptions.jsx
+++ src/pages/ClientOptions.jsx
@@ -154,53 +154,30 @@ function ClientOptionsPage(){
                 <div class="d-flex align-items-stretch" style={{minHeight: "100vh"}}>
                     <div className="text-center" style={{width:"30%" , height:"100vh" , overflowY:"scroll"}}>
                         <h4 className="mt-4 text-info">Waiting</h4>
-                        <div className="d-flex justify-content-center align-items-baseline m-2">
+                        {/* <div className="d-flex justify-content-center align-items-baseline m-2">
                             <h6>eiad sorour</h6>
                             <button className="btn btn-sm ms-1 btn-dark"><i style={{color: "green"}} className="fa-solid fa-circle-check fa-lg"></i></button>
                             <button className="btn btn-sm btn-dark"><i style={{color: "red"}} className="fa-solid fa-circle-xmark fa-lg"></i></button>
-                        </div>
-                        <div className="d-flex justify-content-center align-items-baseline m-2">
-                            <h6>user33</h6>
-                            <button className="btn btn-sm ms-1 btn-dark"><i style={{color: "green"}} className="fa-solid fa-circle-check fa-lg"></i></button>
-                            <button className="btn btn-sm btn-dark"><i style={{color: "red"}} className="fa-solid fa-circle-xmark fa-lg"></i></button>
-                        </div>
+                        </div> */}
                         <hr className="m-1 mt-4"/>
                         <h4 className="mt-4 text-info">In Room</h4>
-                        <div className="d-flex justify-content-center align-items-baseline m-3">
+                        {/* <div className="d-flex justify-content-center align-items-baseline m-3">
                             <h6>user33</h6>
                             <button className="btn btn-danger ms-3">Kick</button>
-                        </div>
-                        <div className="d-flex justify-content-center align-items-baseline m-3">
-                            <h6>ahmed</h6>
-                            <button className="btn btn-danger ms-3">Kick</button>
-                        </div>
-                        <div className="d-flex justify-content-center align-items-baseline m-3">
-                            <h6>Yasser</h6>
-                            <button className="btn btn-danger ms-3">Kick</button>
-                        </div>
+                        </div> */}
                     </div>
 
                     <div style={{width:"100%" , height:"100vh"}}>
                         
                         <div className="text-center" style={{height:"85vh"}}>
-                            <h1 className="pt-2"><span className="text-info">Topic:</span> Computer Science</h1>
+                            <h1 className="pt-2"><span className="text-info">Topic:</span> {createTopic}</h1>
                             <div className="d-flex flex-wrap justify-content-center">
                                 <div className="m-3">
-                                    <h5 className="text-secondary">User 1</h5>
+                                    <h5 className="text-secondary">{user.username}</h5>
                                     <video style={{width:"400px" , height:"220px"}} controls autoplay></video>
                                 </div>
-                                <div className="m-3">
-                                    <h5 className="text-secondary">User 2</h5>
-                                    <video style={{width:"400px" , height:"220px"}} controls autoplay></video>
-                                </div>
-                                <div className="m-3">
-                                    <h5 className="text-secondary">User 3</h5>
-                                    <video style={{width:"400px" , height:"220px"}} controls autoplay></video>
-                                </div>
-                                <div className="m-3">
-                                    <h5 className="text-secondary">User 4</h5>
-                                    <video style={{width:"400px" , height:"220px"}} controls autoplay></video>
-                                </div>
+
+                                {/* Rest of users */}
                             </div>
                         </div>
                         <hr/>
